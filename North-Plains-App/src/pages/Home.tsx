38340a0021c84099cd59418b82304de48ec40f3d
Kobe Davis <kodavis@pdx.edu>
2020-06-22 12:37:43 -0700

    Buttons inside of a Grid inside of Slides
    Rereated the bottom-half UI functionality of the Dayton app. Now we can have icon buttons in a grid-like pattern on the botttom half of the screen. Each grid-like pattern of buttons is embedded in a Slide. This allows us to swipe left or right on it to get a new grid of buttons.

--- North-Plains-App/src/pages/Home.tsx
+++ North-Plains-App/src/pages/Home.tsx
@@ -1,6 +1,26 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonFooter,
+  IonButton,
+  IonIcon,
+  IonSlides,
+  IonSlide
+} from '@ionic/react';
+import { heart, ellipse, square,
+         triangle, planet, bus,
+         notifications, tennisball, gameController,
+         batteryFullOutline, musicalNoteOutline, baseballOutline,
+         playOutline, pauseOutline, stopOutline,
+         fingerPrintOutline, flaskOutline, flameOutline
+} from 'ionicons/icons'
 import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
@@ -8,17 +28,51 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>North Plains</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
+      <IonFooter>
+        <IonSlides>
+          <IonSlide>
+            <IonGrid>
+              <IonRow class="ion-text-center">
+                <IonCol><IonButton><IonIcon icon={batteryFullOutline}/></IonButton></IonCol>
+                <IonCol><IonButton><IonIcon icon={musicalNoteOutline}/></IonButton></IonCol>
+                <IonCol><IonButton><IonIcon icon={baseballOutline}/></IonButton></IonCol>
+              </IonRow>
+              <IonRow class="ion-text-center">
+                <IonCol><IonButton><IonIcon icon={playOutline}/></IonButton></IonCol>
+                <IonCol><IonButton><IonIcon icon={pauseOutline}/></IonButton></IonCol>
+                <IonCol><IonButton><IonIcon icon={stopOutline}/></IonButton></IonCol>
+              </IonRow>
+              <IonRow class="ion-text-center">
+                <IonCol><IonButton><IonIcon icon={fingerPrintOutline}/></IonButton></IonCol>
+                <IonCol><IonButton><IonIcon icon={flaskOutline}/></IonButton></IonCol>
+                <IonCol><IonButton><IonIcon icon={flameOutline}/></IonButton></IonCol>
+              </IonRow>
+            </IonGrid>
+          </IonSlide>
+          <IonSlide>
+            <IonGrid>
+              <IonRow class="ion-text-center">
+                <IonCol><IonButton><IonIcon icon={heart}/></IonButton></IonCol>
+                <IonCol><IonButton><IonIcon icon={ellipse}/></IonButton></IonCol>
+                <IonCol><IonButton><IonIcon icon={square}/></IonButton></IonCol>
+              </IonRow>
+              <IonRow class="ion-text-center">
+                <IonCol><IonButton><IonIcon icon={triangle}/></IonButton></IonCol>
+                <IonCol><IonButton><IonIcon icon={planet}/></IonButton></IonCol>
+                <IonCol><IonButton><IonIcon icon={bus}/></IonButton></IonCol>
+              </IonRow>
+              <IonRow class="ion-text-center">
+                <IonCol><IonButton><IonIcon icon={notifications}/></IonButton></IonCol>
+                <IonCol><IonButton><IonIcon icon={tennisball}/></IonButton></IonCol>
+                <IonCol><IonButton><IonIcon icon={gameController}/></IonButton></IonCol>
+              </IonRow>
+            </IonGrid>
+          </IonSlide>
+        </IonSlides>
+      </IonFooter>
     </IonPage>
   );
 };
