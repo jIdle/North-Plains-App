f0d95b069c4f5193322c2de4ff07d5534356d9e1
Carlos Miguel Sayao <carsayao@pdx.edu>
2020-06-24 20:21:06 -0700

    Sort News.tsx, update capacitor config

--- North-Plains-App/src/pages/News.tsx
+++ North-Plains-App/src/pages/News.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonItem, IonIcon, IonLabel, IonButton } from '@ionic/react';
+import { IonContent,
+         IonHeader,
+         IonPage,
+         IonTitle,
+         IonToolbar,
+         IonCard,
+         IonCardHeader,
+         IonCardSubtitle,
+         IonCardTitle,
+         IonCardContent,
+         IonItem,
+         IonIcon, 
+         IonLabel,
+         IonButton,
+         IonRouterOutlet
+} from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { Redirect, Route } from 'react-router-dom';
 import { pin, wifi, wine, warning, walk } from 'ionicons/icons';
 
 // Test data
@@ -7,11 +24,13 @@ import { pin, wifi, wine, warning, walk } from 'ionicons/icons';
 export const newsItems = [
   {
     title: "COVID-19 Emergency Declaration extended through September 30, 2020",
-    date: "Monday, June 15"
+    date: "Monday, June 15",
+    id: "2"
   },
   {
     title: "Limited Business Hours for City Hall",
-    date: "Sunday, June 14"
+    date: "Sunday, June 14",
+    id: "1"
   }
 ]
 
@@ -26,7 +45,7 @@ const News: React.FC = () => {
       <IonContent>
 
         {newsItems.map((item) => (
-          <IonCard>
+          <IonCard button={true} key={item.id}>
             <IonCardHeader>
               <IonCardSubtitle>
                 {item.date}
